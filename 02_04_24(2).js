0b7db1ca46301a41e27cdb7ae00a92f7c7bf6a0e
Trevor Carmack <tscarmack77@gmail.com>
2024-02-05 19:07:18 -0600

    added digitize cw

---
+++ 02_04_24(2).js
@@ -0,0 +1,8 @@
+// Function 2 - squaring an argument
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// Write the "square"-function here
+function square(x){
+    return x**2
+  }
