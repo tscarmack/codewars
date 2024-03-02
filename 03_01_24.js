06606362a4d578d12844b2a2afe32facaf85ca24
Trevor Carmack <tscarmack77@gmail.com>
2024-03-01 18:33:57 -0600

    added odd or even cw

---
+++ 03_01_24.js
@@ -0,0 +1,21 @@
+// // Odd or even
+
+// Task:
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+function oddOrEven(array) {
+   //enter code here
+  let sum = 0
+  for (let i = 0; i<array.length; i++){
+    sum += array[i]
+  }
+  if(sum % 2 === 0){
+    return 'even'
+  }else{
+    return 'odd'
+  }
+}
